03f2f8518b150951162885ede3fae80f42982613
Kevin Novak <knovak8@gmail.com>
2020-03-31 10:07:42 -0700

    Remove reference to an unused component
    Counter and FetchData were default components, not used by us

--- inventory_manager/ClientApp/src/App.js
+++ inventory_manager/ClientApp/src/App.js
@@ -2,8 +2,6 @@ import React, { Component } from 'react';
 import { Route } from 'react-router';
 import { Layout } from './components/Layout';
 import { Home } from './components/Home';
-import { FetchData } from './components/FetchData';
-import { Counter } from './components/Counter';
 
 import './custom.css'
 import { Inventory } from './components/Inventory';
@@ -15,7 +13,6 @@ export default class App extends Component {
     return (
       <Layout>
         <Route exact path='/' component={Home} />
-        <Route path='/counter' component={Counter} />
         <Route path='/inventory' component={Inventory} />
       </Layout>
     );
